46c1e8a960db668f62cac4db7d54406a01e81108
mebarki <ghanimeba@yahoo.fr>
2019-03-09 02:23:51 +0100

    formInsc

--- src/server/src/main/java/app/etutorat/models/requestobjects/InscriptionForm.java
+++ src/server/src/main/java/app/etutorat/models/requestobjects/InscriptionForm.java
@@ -1,20 +1,15 @@
 package app.etutorat.models.requestobjects;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
 
 public final class InscriptionForm {
 
-    private static final String CHAMP_EMAIL  = "email";
-
-    private static final String CHAMP_PASS   = "motdepasse";
-
-    private static final String CHAMP_CONF   = "confirmation";
-
-    private static final String CHAMP_NOM    = "nom";
+private static final String CHAMP_EMAIL  = "email";
+private static final String CHAMP_PASS   = "motdepasse";
+private static final String CHAMP_CONF   = "confirmation";
+private static final String CHAMP_NOM    = "nom";
 
 
 
