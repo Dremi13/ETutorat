e474e2eb27fbf19baf092f5b0273228b9b9bf9cb
mebarki <ghanimeba@yahoo.fr>
2019-03-12 15:40:51 +0100

    interface d'enregistrement

---
+++ src/client/etutorat-cli/src/app/models/user.models.ts
@@ -0,0 +1,12 @@
+
+export interface Registration
+{
+    Id:number;
+    nom:string;
+    prenom:string;
+    email:string;
+    password:string;
+    codeetu:string; 
+    telephone:number; 
+    filiere:string;
+}
