6108763a25a78343de5e438eda2837c9216b882f
sanaemostadi <47751270+sanaemostadi@users.noreply.github.com>
2019-03-12 20:35:04 +0100

    Add files via upload
    listerSeanceByTuteur et  listerSeanceByTutore

--- src/server/src/main/java/app/etutorat/dao/SeanceRepository.java
+++ src/server/src/main/java/app/etutorat/dao/SeanceRepository.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import app.etutorat.models.Salle;
 import app.etutorat.models.Seance;
 import app.etutorat.models.Tuteur;
+import app.etutorat.models.Tutore;
 
 @Repository
 public interface SeanceRepository extends JpaRepository<Seance, Long> {
@@ -19,10 +20,24 @@ public interface SeanceRepository extends JpaRepository<Seance, Long> {
 	public List<Seance> findByDate(Date date);
 	public List<Seance> findByOutilAV(String outilav);
 	public List<Seance> findBySalle(Salle salle);
+	@Query("SELECT s " 					+
+	           "FROM " 					+
+	           "    Seance s " 			+
+	           "WHEN" 					+
+	           "s.tuteur like  tuteur " +
+	           "GROUP BY " 				+
+	           "    s.id "			)
 	public List<Seance> findByTuteur(Tuteur tuteur);
 	
 	//Sameshit que findByCompetences
-	//public List<Seance> findByTutores(Tutore tutore);
+	@Query("SELECT s " 					+
+	           "FROM " 					+
+	           "    Seance s " 			+
+	           "WHEN" 					+
+	           "s.tutore like  tutore " +
+	           "GROUP BY " 				+
+	           "    s.id "			)
+	public List<Seance> findByTutores(Tutore tutore);
 
 	
 	@Query("SELECT s " 				+
