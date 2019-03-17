2e7557126ed232852f249563c34f62c75a678ca2
sanaemostadi <47751270+sanaemostadi@users.noreply.github.com>
2019-03-17 19:30:06 +0100

    Update SeanceController.java
    correction

--- src/server/src/main/java/app/etutorat/controllers/SeanceController.java
+++ src/server/src/main/java/app/etutorat/controllers/SeanceController.java
@@ -1,50 +1,52 @@
 package app.etutorat.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import app.etutorat.dao.SeanceRepository;
+import app.etutorat.dao.TuteurRepository;
 import app.etutorat.models.Seance;
 import app.etutorat.models.Tuteur;
 import app.etutorat.models.Tutore;
-import app.etutorat.services.AuthentificationService;
-
 import app.etutorat.services.SeanceService;
 
-@Controller
+@RestController
 @RequestMapping("/seance")
 @CrossOrigin(allowCredentials = "true", origins = {"http://localhost:4200"})
 public class SeanceController {
 	
 	 @Autowired
-	  private SeanceService ss;
+	  SeanceService seanceS;
 	 
-	 @Autowired 
-	 Tuteur tuteur;
 	 
-	 @Autowired 
-	 Tutore tutore;
+	 @Autowired
+	  TuteurRepository tuteurR;
 	 
-	 @GetMapping("/tuteur")
-	 public List<Seance> getSeancesByTuteur() {
-			if (tuteur != null) {
-				return ss.findSeanceByTuteur(tuteur);
-			}
-			return null;
-		}
+	 
 	
-	 @GetMapping("/tutore")
-	 public List<Seance> getSeancesByTutore() {
-			if (tutore != null) {
-				return ss.findSeanceByTutore(tutore);
-			}
-			return null;
+	 @GetMapping("/seancetutore")
+	 public  List<Seance> findAllSeanceTutore(Long id) {
+	  return seanceS.listeSeanceTutore(id);
+	 
+	 }
+	 
+	  @GetMapping("/seancetuteur")
+		 public  List<Seance> findAllSeanceTuteur(Long  id) {
+		  return seanceS.listeSeanceTuteur(id);
 		}
+	  
+	  @GetMapping("/all")
+	  public List<Seance> findAllSeances(){
+		  return seanceS.findAllSeances();
+	  }
+	
 
-	
 
 }
