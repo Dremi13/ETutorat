19cc53ff8d047ec82f2e551685a333aba3d37a8d
sanaemostadi <47751270+sanaemostadi@users.noreply.github.com>
2019-03-17 19:28:19 +0100

    Update SeanceService.java
    correction

--- src/server/src/main/java/app/etutorat/services/SeanceService.java
+++ src/server/src/main/java/app/etutorat/services/SeanceService.java
@@ -1,36 +1,61 @@
 package app.etutorat.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import app.etutorat.dao.SeanceRepository;
+import app.etutorat.dao.TuteurRepository;
+import app.etutorat.dao.TutoreRepository;
 import app.etutorat.models.Seance;
 import app.etutorat.models.Tuteur;
 import app.etutorat.models.Tutore;
+import app.etutorat.models.queryobjects.PaireStringLong;
 
 
 @Service
-@Transactional
+
 public class SeanceService {
 	
-	@Autowired
-	private SeanceRepository seanceR;
+	 @Autowired
+	    public TuteurRepository tuteurR;
+	 
+	 @Autowired
+	    public TutoreRepository tutoreR;
+	    
+	 @Autowired
+	 	public SeanceRepository seanceR;
+	
+	 
+	 public List<Seance> listeSeanceTutore(Long id)
+	 {
+		
+		return seanceR.findByIdTutore(id);
+		}
+	 
+	 public List<Seance> listeSeanceTuteur(Long id)
+	 {
 
-	
-	public List<Seance> findSeanceByTuteur(Tuteur tuteur)
-	{
-		return seanceR.findByTuteur(tuteur);
+		return seanceR.findByIdTuteur(id);
 	}
-	
-	public List<Seance> findSeanceByTutore(Tutore tutore)
-	{
-		return seanceR.findByTutores(tutore);
-	}
-	
 
+	 public List<Seance> findSeancesDisponibles(){
+	
+		
+		 return seanceR.findSeancesDisponibles();
+		 
+	 }
+	 
+	 public List<Seance> findAllSeances(){
+		 return seanceR.findAllSeances();
+	 }
+  
+  
 }
